562498343d5fcaa2ed5d9ed4e64ced5f38f6be2f
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2025-02-14 17:03:25 +0530

    improved sidebar

--- src/components/shared/sidenav/SideNav.tsx
+++ src/components/shared/sidenav/SideNav.tsx
@@ -1,6 +1,42 @@
-const SideNav = () => {
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail } from "@/components/ui/sidebar"
+import React from "react"
+import { NavMain } from "./nav-main"
+import { NAV_ITEMS } from "./nav-items.const"
+
+const SideNav = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
   return (
-    <div className="p-2">SideNav</div>
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-4xl">
+              🍍
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  Pineapple
+                </span>
+                {/* <span className="truncate text-xs">{activeTeam.plan}</span> */}
+              </div>
+              {/* <ChevronsUpDown className="ml-auto" /> */}
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        {/* <TeamSwitcher teams={data.teams} /> */}
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={NAV_ITEMS.navMain} />
+        {/* <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        {/* <NavUser user={data.user} /> */}
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
   )
 }
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/components/shared/sidenav/SideNav'
 import { Button } from '@/components/ui/button'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
@@ -7,23 +8,36 @@ export const Route = createRootRoute({
 })
 
 function RootComponent() {
+  const isAuth = true;
+
+  if(isAuth) {
+    return (
+      <SidebarProvider>
+        <SideNav />
+        <SidebarInset>
+          <div className='flex flex-col w-full h-full p-4'>
+            <SidebarTrigger className="-ml-1" />
+            <div className='text-lg'>This is __root Pineapple AI 🍍</div>
+            <div className='flex space-x-4 px-4'>
+              <Link to='/'>
+                <Button variant={'link'} size={'sm'}>
+                Home
+                </Button>
+              </Link>
+              <Link to='/dashboard'>Dashboard</Link>
+              <Link to='/event-analyzer'>/event-analyzer</Link>
+              <Link to='/product-registration'>/product-registration</Link>
+            </div>
+            <Outlet />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    )
+  }
+
   return (
-    <div className='flex w-full h-screen'>
-      <SideNav />
-      <div className='flex flex-col w-full h-full p-4'>
-        <div className='text-lg'>This is __root Pineapple AI 🍍</div>
-        <div className='flex space-x-4 px-4'>
-          <Link to='/'>
-            <Button variant={'link'} size={'sm'}>
-            Home
-            </Button>
-          </Link>
-          <Link to='/dashboard'>Dashboard</Link>
-          <Link to='/event-analyzer'>/event-analyzer</Link>
-          <Link to='/product-registration'>/product-registration</Link>
-        </div>
-        <Outlet />
-      </div>
+    <div>
+      no auth
     </div>
   )
 }
